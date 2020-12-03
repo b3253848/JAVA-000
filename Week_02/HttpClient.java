0bb14ded9a01811a3085c726765ea7e23cf10965
zhaohl <zhaohaoliang@csii.com.cn>
2020-12-03 20:15:08 +0800

    make up for the week02's homework missed

---
+++ Week_02/HttpClient.java
@@ -0,0 +1,55 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClient {
+
+    public static void main(String[] args) {
+        // 获得Http客户端
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        // 创建Post请求
+        HttpPost httpPost = new HttpPost("https://www.baidu.com/");
+
+        // 设置ContentType
+        httpPost.setHeader("Content-Type", "application/json;charset=utf8");
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端发送Post请求
+            response = httpClient.execute(httpPost);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                // 指定字符集，避免中文乱码
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity, "UTF-8"));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
